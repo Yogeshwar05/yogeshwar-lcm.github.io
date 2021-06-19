c51acba3a0accfe2a6289430a713d08400f963f1
Yogeshwar Jhariya <yogeshwarjhariya2020@gmail.com>
2021-06-19 18:06:47 +0530

    Half Website Done

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ console.log(`Welcome to Yogeshwar's Website`);
 let no1 = document.getElementById(`card1`);
 let no2 = document.getElementById(`card2`);
 document.getElementById(`addBtn`).addEventListener(`click`, function (e) {
-    let a = no1.value
-    let b = no2.value
+    let a = Number(no1.value)
+    let b = Number(no2.value)
+    console.log(typeof (a));
     let x = (a > b) ? a : b;
-
-    while (true) {
-        if (x % a == 0 && x % b == 0) {
-            document.getElementById('ans').innerText = x
-            break;
+    if (typeof (a) == `number` && typeof (b) == `number`) {
+        while (true) {
+            if (x > 10000) {
+                document.getElementById('ans').innerText = `Error! Very Big Value`;
+                break;
+            }
+            else if (x % a == 0 && x % b == 0) {
+                document.getElementById('ans').innerText = x;
+                break;
+            }
+            x++
         }
-        x++
+
+    }
+    else {
+        document.getElementById('ans').innerText = `Error!`;
+        // break;
     }
     no1.value = ""
     no2.value = ""
-})
+}
+)
+// console.log(typeof (20));
